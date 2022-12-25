ece9b7fd30900c418b386cfbe9f5b2f319138574
Lee Santos <lee.m.santos@outlook.com>
2022-12-25 01:10:19 -0500

    Bug fix on auth.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 require("dotenv").config();
 console.log(process.env)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const key = process.env.MAILCHIMP_API;
 
 app.use(express.static("public"));
